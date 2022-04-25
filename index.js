3e6a58a04a2a9434f4ad4b55a1668cb376642ce8
arnobtarafder <rmtarnob@gmail.com>
2022-04-25 19:50:18 +0600

    orders section by email address

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("service");
+        const orderCollection = client.db("geniusCar").collection("order");
 
         // SERVICES API
         app.get("/service", async (req, res) => {
@@ -47,6 +48,22 @@ async function run() {
             const query = {_id: ObjectId(id)};
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // Order Collection API
+
+        app.get("/order", async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.post("/order", async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
         })
     }
     finally {
