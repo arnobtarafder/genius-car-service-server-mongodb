10ded6c25cfabbf40abbfb9c4993059bb4517906
arnobtarafder <rmtarnob@gmail.com>
2022-04-26 16:29:04 +0600

    adding steps

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jwt');
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 520;
@@ -9,6 +9,25 @@ const port = process.env.PORT || 520;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" })
+        }
+        // console.log("decoded", decoded);
+        req.decoded = decoded;
+        next();
+    })
+    // console.log("inside verifyJWT", authHeader);
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.humt7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -20,12 +39,12 @@ async function run() {
         const orderCollection = client.db("geniusCar").collection("order");
 
         // AUTH
-        app.post("/login", async(req, res) => {
+        app.post("/login", async (req, res) => {
             const user = req.body;
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expireIn: "Id"
+                expiresIn: "1d"
             });
-          res.send({accessToken})  
+            res.send({ accessToken })
         })
 
         // SERVICES API
@@ -36,41 +55,47 @@ async function run() {
             res.send(services)
         });
 
-        app.get("/service/:id", async(req, res) => {
+        app.get("/service/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             // console.log(query);
             // console.log(ObjectId);
             res.send(service);
         })
-        
+
         //POST
-        app.post("/service", async(req, res) => {
+        app.post("/service", async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result)
         })
 
         // DELETE
-        app.delete("/service/:id", async(req, res) => {
+        app.delete("/service/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         })
 
         // Order Collection API
 
-        app.get("/order", async(req, res) => {
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = {email: email};
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
 
-        app.post("/order", async(req, res) => {
+        app.post("/order", async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result)
